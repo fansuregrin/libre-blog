9e31bfeddf5571f1dc0d262b42baa4d5bb61eb69
fansuregrin <pwz113436@gmail.com>
2024-05-09 13:35:06 +0000

    Update gen_users.sql to fix some errors

--- sql/gen_users.sql
+++ sql/gen_users.sql
@@ -1,12 +1,18 @@
+-- 拥有管理员(administrator)权限的用户
 SET @uuid = UUID();
 INSERT INTO `user` (`username`,`password`,`email`,`realname`,`salt`,`role`) VALUES
     ('bobwood', UPPER(SHA2(CONCAT('bw1234', @uuid),256)), 'bobwood@ouc.edu.cn', 'Bob Wood', @uuid, 1);
+
+-- 其他权限的用户
 SET @uuid = UUID();
-INSERT INTO `user` (`username`,`password`,`email`,`realname`,`salt`) VALUES
-    ('yuhua', UPPER(SHA2(CONCAT('xiaohua1234', @uuid),256)), 'xiaohua@bnu.edu.cn', '小华', @uuid, 2);
-INSERT INTO `user` (`username`,`password`,`email`,`realname`,`salt`) VALUES
-    ('moyan', UPPER(SHA2(CONCAT('xiaomo1234', @uuid),256)), 'xiaoyan@bnu.edu.cn', '小莫', @uuid, 2);
-INSERT INTO `user` (`username`,`password`,`email`,`realname`,`salt`) VALUES
+INSERT INTO `user` (`username`,`password`,`email`,`realname`,`salt`, `role`) VALUES
+    ('xiaohua', UPPER(SHA2(CONCAT('xiaohua1234', @uuid),256)), 'xiaohua@bnu.edu.cn', '小华', @uuid, 2);
+SET @uuid = UUID();
+INSERT INTO `user` (`username`,`password`,`email`,`realname`,`salt`, `role`) VALUES
+    ('xiaomo', UPPER(SHA2(CONCAT('xiaomo1234', @uuid),256)), 'xiaoyan@bnu.edu.cn', '小莫', @uuid, 2);
+SET @uuid = UUID();
+INSERT INTO `user` (`username`,`password`,`email`,`realname`,`salt`, `role`) VALUES
     ('tom1997', UPPER(SHA2(CONCAT('ts1234', @uuid),256)), 'ts@gmail.com', 'Tom Smith', @uuid, 3);
-INSERT INTO `user` (`username`,`password`,`email`,`realname`,`salt`) VALUES
+SET @uuid = UUID();
+INSERT INTO `user` (`username`,`password`,`email`,`realname`,`salt`, `role`) VALUES
     ('dijia', UPPER(SHA2(CONCAT('dj6666', @uuid),256)), 'dijia@m78.galaxy', '迪迪', @uuid, 4);
